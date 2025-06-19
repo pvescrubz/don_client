196782bcba0d868422b0adf80d86c4e4fa1266b8
qseven <qseven2012@gmail.com>
2025-06-19 23:03:48 +0300

    fix blog

--- src/shared/components/sections/Blog/BlogSlider/BlogSlider.tsx
+++ src/shared/components/sections/Blog/BlogSlider/BlogSlider.tsx
@@ -3,17 +3,24 @@ import { ButtonLink } from "@/shared/components/ui/Button/ButtonLink";
 import { Section } from "@/shared/components/ui/containers/Section/Section";
 import { SliderPosts } from "@/shared/components/ui/Slider/SliderPost";
 import { Title } from "@/shared/components/ui/Title/Title";
-import { BlogPostsProps } from "@/shared/typing/blog.type";
+
+import { BlogConfig } from "@/shared/content/blog.config";
 import { BlogItem } from "../BlogItem/BlogItem";
 import styles from "./BlogSlider.module.css";
 
 
-export const BlogSlider  = ({ posts }: BlogPostsProps) => {
+export const BlogSlider  = () => {
+    const posts = Object.values(BlogConfig);
+  
   return (
     <Section className={styles.root}>
       <Title>Блог</Title>
       <SliderPosts height="350px" cardWidth={350} gap={20} autoScrollSpeed={1000}>
-          <BlogItem posts={posts}/>
+          
+            {posts.map((post) => (
+              <BlogItem key={post.name} post={post} />
+            ))}
+          
       </SliderPosts>
       <ButtonLink href="/blog" size="large" variant="primary" className={styles.button}>Все статьи</ButtonLink>
     </Section>
