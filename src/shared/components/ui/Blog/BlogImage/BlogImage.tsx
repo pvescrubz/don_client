22b8ae69a73436b71616dc7a9068cc1740086b74
Дмитрий Кубарев <kubarevds@icloud.com>
2025-06-19 21:51:36 +0300

    fix blog

--- src/shared/components/ui/Blog/BlogImage/BlogImage.tsx
+++ src/shared/components/ui/Blog/BlogImage/BlogImage.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
 
-
 import { FC } from "react";
 import styles from "./BlogImage.module.css";
 
@@ -13,12 +12,8 @@ interface Props {
 
 export const BlogImage: FC<Props> = ({ data }) => {
   return (
-    <div className={styles.imageWrapper}>
-      <img
-        src={data.content}
-        alt=""
-        className={styles.image}
-      />
+    <div className={styles.root}>
+      <img src={data.content} alt="" className={styles.image} />
     </div>
   );
 };
