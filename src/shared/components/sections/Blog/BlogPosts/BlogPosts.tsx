3d042514d1a5f59859604e22fdde2c9dab0d8679
qseven <qseven2012@gmail.com>
2025-06-19 22:23:42 +0300

    fix

--- src/shared/components/sections/Blog/BlogPosts/BlogPosts.tsx
+++ src/shared/components/sections/Blog/BlogPosts/BlogPosts.tsx
@@ -1,5 +1,6 @@
 // components/BlogPosts.tsx
 
+import { Breadcrumbs } from "@/shared/components/ui/BreadCrums/BreadCrums";
 import { Container } from "@/shared/components/ui/containers/Container/Container";
 import { Section } from "@/shared/components/ui/containers/Section/Section";
 import { IBlogArticle } from "@/shared/typing/blog.type";
@@ -12,10 +13,14 @@ interface BlogPostsProps {
 
 export const BlogPosts = ({ posts }: BlogPostsProps) => {
   return (
+    <>
+ <Breadcrumbs />
     <Section className={styles.root}>
       <Container className={styles.container}>
         <BlogItem posts={posts}/>
       </Container>
     </Section>
+</>
+   
   );
 };
